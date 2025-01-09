c084ccfd0a8eb3ca9c06640322ccc3d14c5b8ad3
s-celles <s.celles@gmail.com>
2025-01-09 07:37:46 +0100

    initial commit
    Signed-off-by: s-celles <s.celles@gmail.com>

---
+++ src/main.zig
@@ -0,0 +1,21 @@
+pub fn fib(n: i64) i64 {
+    if (n <= 2) return 1;
+    if (n == 3) return 2;
+    return fib(n - 1) + fib(n - 2);
+}
+
+pub fn main() !void {
+    const std = @import("std");
+
+    const timer = std.time.Timer;
+    var t = try timer.start();
+
+    const n: i64 = 50;
+    const result = fib(n);
+
+    const elapsed = t.read();
+    const seconds = @as(f64, @floatFromInt(elapsed)) / std.time.ns_per_s;
+
+    std.debug.print("Time elapsed: {d:.6} seconds\n", .{seconds});
+    std.debug.print("fib({}) = {}\n", .{ n, result });
+}
